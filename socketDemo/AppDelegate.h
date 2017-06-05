b6786f1d3946bc6206744063215a1d28c6e81bd0
紫川秀 <tszy111@126.com>
2017-06-05 15:28:38 +0800

    Initial Commit

---
+++ socketDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  socketDemo
+//
+//  Created by 李自杨 on 2017/6/5.
+//  Copyright © 2017年 View. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ socketDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  socketDemo
+//
+//  Created by 李自杨 on 2017/6/5.
+//  Copyright © 2017年 View. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
